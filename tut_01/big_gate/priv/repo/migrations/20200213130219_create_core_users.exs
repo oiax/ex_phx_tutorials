93a789b424b2e5126a2311cdac31a35dff5c6abe
Tsutomu Kuroda <t-kuroda@oiax.jp>
2020-02-13 22:08:10 +0900

    [01] Add name and email fields to core_users table

--- tut_01/big_gate/priv/repo/migrations/20200213130219_create_core_users.exs
+++ tut_01/big_gate/priv/repo/migrations/20200213130219_create_core_users.exs
@@ -3,9 +3,12 @@ defmodule BigGate.Repo.Migrations.CreateCoreUsers do
 
   def change do
     create table(:core_users) do
+      add(:name, :string, null: false)
+      add(:email, :string, null: false)
 
       timestamps()
     end
 
+    create(unique_index(:core_users, :name))
   end
 end
